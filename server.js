94eeed1695b0a02b98efed2bf31f2cf39aa92b9a
liadna1995 <liadna1995@gmail.com>
2022-04-04 16:39:27 +0300

    Run Researchers homepage on port 5000

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var path = require('path');
 var serveStatic = require('serve-static');
 var app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 443;
+var port = process.env.PORT || 5000;
 app.listen(port);
-// console.log('server started '+ port);
+console.log('server started '+ port);
 
 // console.log(config_vue);
 
--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ config.isProduction = true // Real mode: true
 /* For serverCommunicator */
 // config.serverUrl = "https://twixper-backend.azurewebsites.net"
 config.serverUrl = "https://rps.ise.bgu.ac.il/njsw31"
-// config.serverUrl = "http://localhost:3000"
+// config.serverUrl = "http://localhost:443"
 
 config.isTest = false // Real mode: false
 config.isTestingErrors = true
