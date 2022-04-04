27ed801dab649d81db68d35bd5ec5b8934292af4
liadna1995 <liadna1995@gmail.com>
2022-04-04 16:08:14 +0300

    try to fix server conction

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
+var app = express();
 app.use(serveStatic(__dirname + "/dist"));
-// var port = process.env.PORT || 443;
-// app.listen(port);
+var port = process.env.PORT || 443;
+app.listen(port);
 // console.log('server started '+ port);
 
 // console.log(config_vue);
--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ config.isProduction = true // Real mode: true
 
 /* For serverCommunicator */
 // config.serverUrl = "https://twixper-backend.azurewebsites.net"
-// config.serverUrl = ""
-config.serverUrl = "http://localhost:3000"
+config.serverUrl = "https://rps.ise.bgu.ac.il/njsw31"
+// config.serverUrl = "http://localhost:3000"
 
 config.isTest = false // Real mode: false
 config.isTestingErrors = true
