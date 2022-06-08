570b8e8c725b119b0086e65b192e6711d40f2836
liadna1995 <liadna1995@gmail.com>
2022-06-08 18:43:10 +0300

    try to deploy changes

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+
+// configure Https
+var fs = require('fs');
+var privateKey  = fs.readFileSync('C:\\Program Files\\Git\\usr\\bin\\privateKey.key', 'utf8');
+var certificate = fs.readFileSync('C:\\Program Files\\Git\\usr\\bin\\certificate.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+var https = require('https');
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443, () => {
+  console.log(`Server running at https://localhost:443/`);
+});
