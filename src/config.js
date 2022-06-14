01aebec95e13dcda65864235586b457297aa6d52
danieldinshtein <danield@blinkops.com>
2022-06-15 01:19:57 +0300

    change ServerUrl

--- src/config.js
+++ src/config.js
@@ -1,14 +1,13 @@
-var config = new Object()
+var config = new Object();
 
 // For choosing "download report" flow
-config.isProduction = true // Real mode: true
+config.isProduction = true; // Real mode: true
 
 /* For serverCommunicator */
-// config.serverUrl = "https://twixper-backend.azurewebsites.net"
-config.serverUrl = "http://localhost:3000"
+config.serverUrl = "https://rps.ise.bgu.ac.il/njsw31";
+// config.serverUrl = "http://localhost:3000"
 
-config.isTest = false // Real mode: false
-config.isTestingErrors = true
-
+config.isTest = false; // Real mode: false
+config.isTestingErrors = true;
 
 module.exports = config;
